d6a10a1e3fff7f41c008ff98260cb3486944f920
isspek <isspek@users.noreply.github.com>
2016-05-30 21:06:40 +0300

    Add files via upload

--- key-detection.py
+++ key-detection.py
@@ -1,34 +1,68 @@
-'''
-    File name: key-detection.py
-    Author: isspek
-    Date created: 22/05/2016
-    Date last modified: 
-    Python Version: 2.7.6
-'''
+import essentia
+from essentia.streaming import *
+from essentia import Pool
 import os
-import numpy
-import scipy.io.wavfile as wav
+from pylab import plot, show, figure, imshow
 
 
 
-"""
-Reading the audio files as .wav format, and storing their rate and data in an array.
-"""
+def extract_HPCP(_filename):
+    
+    # initialization
 
-class Audio:
-    def __init__(self, rate, data):
-        self.rate=rate
-        self.data=data
+    pool = Pool()
+    loader = MonoLoader(filename = _filename)
+    frameCutter = FrameCutter(frameSize = 4096, hopSize = 512)
+    window = Windowing(type = 'blackmanharris62')
+    spectrum = Spectrum()
+    spectralPeaks= SpectralPeaks(orderBy="magnitude",
+                              magnitudeThreshold=1e-05,
+                              minFrequency=100.0,
+                              maxFrequency=5000.0, 
+                              maxPeaks=10000)
+    hpcp = HPCP(size = 36,
+                   bandPreset = False,
+                   minFrequency = 100.0,
+                   maxFrequency = 5000.0,
+                   weightType = 'squaredCosine',
+                   nonLinear = False,
+                   normalized=True,
+                   sampleRate= 44100.0,
+                   windowSize = 4.0/3.0)
 
-def create_audio_objs(path):
-    rate, data= wav.read(path,0)
-    return Audio(rate,data)
+    # audio-->frame cutter-->windowing-->spectrum-->spectral peaks-->HPCP 
 
-audioArray=[]
+    loader.audio >> frameCutter.signal
+    frameCutter.frame >> window.frame >> spectrum.frame
+    spectrum.spectrum >> spectralPeaks.spectrum
+    spectralPeaks.magnitudes >> hpcp.magnitudes
+    spectralPeaks.frequencies >> hpcp.frequencies
+    hpcp.hpcp >> (pool, 'hpcp vector')
+    # run streaming algorithms
+    essentia.run(loader)
+    return pool['hpcp vector']
+
+paths=[]
 
 for dirname, dirnames, filenames in os.walk('./mirex_key/audio'):
     for filename in sorted(filenames):
-        audioArray.append(create_audio_objs(os.path.join(dirname, filename)));
+        paths.append(os.path.join(dirname, filename))
 
-for i in range(len(audioArray)):
-    print audioArray[i].data
+features=extract_HPCP(paths[18])
+imshow(features,aspect = 'auto')
+show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
