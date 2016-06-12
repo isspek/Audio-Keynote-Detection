f0098fef11d99a5b6c3b00f4b3b1c27875cb0cd6
isspek <ipekbrs@gmail.com>
2016-06-12 22:27:48 +0300

    svm classification

--- key-detection.py
+++ key-detection.py
@@ -3,13 +3,23 @@ from essentia.streaming import *
 from essentia import Pool
 import os
 from pylab import plot, show, figure, imshow
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.cross_validation import train_test_split
 
+def normalize(hpcp):
+    m = max(hpcp)
+    for i in range(len(hpcp)):
+        hpcp[i] = hpcp[i] / m
+    return hpcp
 
 def extract_HPCP(_filename):
     
     # initialization
-
+    HPCP_size=36
     pool = Pool()
+    key = Key()
     loader = MonoLoader(filename = _filename)
     frameCutter = FrameCutter(frameSize = 4096, hopSize = 512)
     window = Windowing(type = 'blackmanharris62')
@@ -19,13 +29,12 @@ def extract_HPCP(_filename):
                               minFrequency=100.0,
                               maxFrequency=5000.0, 
                               maxPeaks=10000)
-    hpcp = HPCP(size = 36,
+    hpcp = HPCP(size = HPCP_size,
                    bandPreset = False,
                    minFrequency = 100.0,
                    maxFrequency = 5000.0,
                    weightType = 'squaredCosine',
                    nonLinear = False,
-                   normalized=True,
                    sampleRate= 44100.0,
                    windowSize = 4.0/3.0)
 
@@ -36,28 +45,42 @@ def extract_HPCP(_filename):
     spectrum.spectrum >> spectralPeaks.spectrum
     spectralPeaks.magnitudes >> hpcp.magnitudes
     spectralPeaks.frequencies >> hpcp.frequencies
-    hpcp.hpcp >> (pool, 'hpcp vector')
-    # run streaming algorithms
+    hpcp.hpcp>> (pool, 'hpcp vector')
     essentia.run(loader)
-    return pool['hpcp vector']
+    hpcpVector= pool['hpcp vector']
+    globalHPCP=[]
+    #taking mean value for each frame to get global HPCP 
+    globalHPCP=hpcpVector.mean(axis=0) 
+    #normalized global HPCP
+    globalHPCP=normalize(globalHPCP)
+    return globalHPCP
 
-
-ground_truth=[]
-features=[]
+data=[]
+labels=[]
 
 for dirname, dirnames, filenames in os.walk('./mirex_key'):
     for filename in sorted(filenames):
         if dirname=='./mirex_key/audio':
-            features.append(extract_HPCP(os.path.join(dirname, filename)))
+            data.append(extract_HPCP(os.path.join(dirname, filename)))
         else:
             file = open(os.path.join(dirname, filename), 'r')
-            ground_truth.append(file.read())
+            labels.append(file.read())
+
+# split 60% of data as train, 40% of data as test
+dataTrain, dataTest, labelTrain, labelTest = train_test_split(data,labels, test_size=0.4, random_state=0)
 
 
+predictions=OneVsRestClassifier(LinearSVC(random_state=0)).fit(dataTrain,labelTrain).predict(dataTest)
+true=0.0
+labelLen=len(labelTest)
+for j in range(0,labelLen):
+    if predictions[j] == labelTest[j]:
+        true=true+1
 
+accuracy=true/labelLen
 
-
-
+print accuracy
+print labelLen
 
 
 
